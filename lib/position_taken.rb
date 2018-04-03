6df9600bc6b3bcbd5f32177db5878d30855df361
Mike C <mikecronin92@gmail.com>
2018-04-03 02:42:50 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,10 @@
 # code your #position_taken? method here!
 
+<<<<<<< HEAD
 def position_taken?(board, ind) 
   return board[ind] == "X" || board[ind] == "O" ? true : false
+=======
+def position_taken?(board, index) 
+  return true if board[index].downcase == "x" || board[index].downcase == "o"
+>>>>>>> 2a94fe509493a7a48e508a272711e608702759c1
 end
